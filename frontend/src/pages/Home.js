d46f6c075cb676522857af2a7b2ee86f0ed57502
yogeshwera96 <168747660+yogeshwera96@users.noreply.github.com>
2024-08-14 17:07:29 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts',{
+      const response = await fetch('https://mern-1-0uxu.onrender.com',{
         headers:{
           'Authorization': `Bearer ${user.token}`
         }
