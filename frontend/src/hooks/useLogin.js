6cdb0938435cc87697e97f6b5ab7b158db51bd6e
yogeshwera96 <168747660+yogeshwera96@users.noreply.github.com>
2024-08-16 12:00:35 +0530

    Update useLogin.js

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -10,7 +10,7 @@ export const useLogin = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('https://mern-nhxb.onrender.com/user/login', {
+    const response = await fetch('https://mern-nhxb.onrender.com', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
