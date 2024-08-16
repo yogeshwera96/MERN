1f0496451266c5cd7148d6bdf47428d03eb66c42
yogeshwera96 <168747660+yogeshwera96@users.noreply.github.com>
2024-08-16 11:06:45 +0530

    Update WorkoutForm.js

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -22,7 +22,7 @@ const WorkoutForm = () => {
 
     const workout = {title, load, reps}
 
-    const response = await fetch('https://mern-nhxb.onrender.com/api/workouts', {
+    const response = await fetch('https://mern-nhxb.onrender.com/workouts', {
       method: 'POST',
       body: JSON.stringify(workout),
       headers: {
