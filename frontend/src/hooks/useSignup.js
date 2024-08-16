92839551aa218aa8a4892e9fcc6ef4d2825a1122
yogeshwera96 <168747660+yogeshwera96@users.noreply.github.com>
2024-08-16 12:00:58 +0530

    Update useSignup.js

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -10,7 +10,7 @@ export const useSignup = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('https://mern-nhxb.onrender.com/user/signup', {
+    const response = await fetch('https://mern-nhxb.onrender.com', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
